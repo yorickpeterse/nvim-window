a66f8590e13483002f31ae6eed725c0f607a8f57
Yorick Peterse <yorick@yorickpeterse.com>
2021-07-07 17:37:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# nvim-window
+
+A simple and opinionated NeoVim plugin for switching between windows in the
+current tab page.
+
+In (Neo)Vim you can navigate between windows using `Control+W`, followed by a
+motion. For example, to move to the window on the right of the current one you'd
+press `Control+W l`. This works for simple scenarios, but gets annoying fast.
+Consider the following window layout:
+
+```
++---+---+---+---+
+|   |   |   |   |
+| A | B | C | D |
+|   |   |   |   |
++---+---+---+---+
+```
+
+Assuming window A is the current window your cursor is in, and you want to move
+to window C, you might press `Control+W 2l`. This is doable, but having to count
+the number of steps every time gets tedious.
+
+It gets more annoying when you want to move to a window above your current one,
+but the window is on the right:
+
+```
++---+---+---+
+|   |   |   |
+| A | B | C |
+|   |   |   |
++---+---+---+
+|     D     |
++-----------+
+```
+
+Assuming window D is active and you want to move to window B, you'd have to take
+two steps:
+
+- `Control+W k` to move to window A
+- `Control+W l` to move to window B
+
+While there may be other ways of doing this, the use of `Control+W` is probably
+the easiest approach to remember; and it's still annoying.
+
+This is where nvim-window comes in: it makes it trivial to jump between windows
+(in the current tab page), without the need for knowing how far away they are or
+relying on a fuzzy finder (and having to type out the window paths).
+
+Using nvim-window, press your binding of choice to activate nvim-window, press
+one letter (or two, if there are many windows), and the given window becomes
+active. This makes it _much_ easier to quickly jump between windows.
+
+You can see this in action here:
+
+[![nvim-window in action](screencast.png)](https://asciinema.org/a/424318)
+
+## Requirements
+
+NeoVim 0.5 or newer
+
+## Installation
+
+First add this plugin using your plugin manager of choice. For example, using
+vim-plug:
+
+```vim
+Plug 'https://gitlab.com/yorickpeterse/nvim-window.git'
+```
+
+For the exact steps necessary, please refer to your plugin manager's
+documentation.
+
+Once installed you need to add a key binding, as nvim-window doesn't define any
+key bindings for you. You can set up a binding like so:
+
+```vim
+map <silent> <leader>w :lua require('nvim-window').pick()<CR>
+```
+
+## Configuration
+
+The default settings should be good enough for most. In case you want to change
+them (e.g. by changing the background of the floating window), you can do so as
+follows:
+
+```lua
+require('nvim-window').setup({
+  -- The characters available for hinting windows.
+  chars = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  },
+
+  -- A group to use for overwriting the Normal highlight group in the floating
+  -- window. This can be used to change the background color.
+  normal_hl = 'Normal',
+
+  -- The highlight group to apply to the line that contains the hint characters.
+  -- This is used to make them stand out more.
+  hint_hl = 'Bold',
+
+  -- The border style to use for the floating window.
+  border = 'single'
+})
+```
+
+You can place this in your `init.lua` before or after defining the mapping. You
+can also dump it in a different Lua file; just make sure to actually load that
+file :)
+
+If you want to replicate the style from the recording, use the following
+settings:
+
+```lua
+require('nvim-window').setup({
+  normal_hl = 'BlackOnLightYellow',
+  hint_hl = 'Bold',
+  border = 'none'
+})
+```
+
+You then have to define the `BlackOnLightYellow` highlight group somewhere using
+these settings:
+
+```vim
+hi BlackOnLightYellow guifg=#000000 guibg=#f2de91
+```
+
+The theme used is [vim-paper](https://gitlab.com/yorickpeterse/vim-paper), which
+has this highlight group built-in.
+
+## License
+
+All source code in this repository is licensed under the Mozilla Public License
+version 2.0, unless stated otherwise. A copy of this license can be found in the
+file "LICENSE".
---
+++ lua/nvim-window.lua
@@ -0,0 +1,143 @@
+-- A simple and opinionated NeoVim plugin for switching between windows in the
+-- current tab page.
+local api = vim.api
+local fn = vim.fn
+local M = {}
+
+-- The keycode for the Escape key, used to cancel the window picker.
+local escape = 27
+
+-- For the sake of keeping this plugin simple, we don't support changing the
+-- dimensions of the floating window.
+local float_height = 3
+local float_width = 6
+
+local config = {
+  -- The characters available for hinting windows.
+  chars = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  },
+
+  -- A group to use for overwriting the Normal highlight group in the floating
+  -- window. This can be used to change the background color.
+  normal_hl = 'Normal',
+
+  -- The highlight group to apply to the line that contains the hint characters.
+  -- This is used to make them stand out more.
+  hint_hl = 'Bold',
+
+  -- The border style to use for the floating window.
+  border = 'single'
+}
+
+local function is_regular_window(winid)
+  return api.nvim_win_get_config(winid).relative == ''
+end
+
+-- Returns a table that maps the hint keys to their corresponding windows.
+local function window_keys(windows)
+  local mapping = {}
+  local chars = config.chars
+
+  for _, win in ipairs(windows) do
+    local key = chars[(win % #chars) + 1]
+
+    if mapping[key] then
+      key = key .. chars[((win + 1) % #chars) + 1]
+    end
+
+    mapping[key] = win
+  end
+
+  return mapping
+end
+
+-- Returns true if we need to ask for a second character.
+local function ask_second_char(keys, start)
+  for key, _ in pairs(keys) do
+    if key ~= start and key:sub(1, 1) == start then
+      return true
+    end
+  end
+
+  return false
+end
+
+-- Opens all the floating windows in (roughly) the middle of every window.
+local function open_floats(mapping)
+  local floats = {}
+
+  for key, window in pairs(mapping) do
+    local bufnr = api.nvim_create_buf(false, true)
+
+    if bufnr > 0 then
+      local win_width = api.nvim_win_get_width(window)
+      local win_height = api.nvim_win_get_height(window)
+
+      local row = math.max(0, math.floor((win_height / 2) - 1))
+      local col = math.max(0, math.floor((win_width / 2) - float_width))
+
+      api.nvim_buf_set_lines(bufnr, 0, -1, true, { '', '  ' .. key .. '  ', '' })
+      api.nvim_buf_add_highlight(bufnr, 0, config.hint_hl, 1, 0, -1)
+
+      local float_window = api.nvim_open_win(bufnr, false, {
+        relative = 'win',
+        win = window,
+        row = row,
+        col = col,
+        width = #key == 1 and float_width - 1 or float_width,
+        height = float_height,
+        focusable = false,
+        style = 'minimal',
+        border = config.border,
+        noautocmd = true
+      })
+
+      api.nvim_win_set_option(float_window, 'winhl', 'Normal:' .. config.normal_hl)
+
+      floats[float_window] = bufnr
+    end
+  end
+
+  return floats
+end
+
+-- Configures the plugin by merging the given settings into the default ones.
+function m.setup(user_config)
+  config = vim.tbl_extend('force', config, user_config)
+end
+
+-- Picks a window to jump to, and makes it the active window.
+function M.pick()
+  local windows =
+    vim.tbl_filter(is_regular_window, api.nvim_tabpage_list_wins(0))
+
+  local window_keys = window_keys(windows)
+  local floats = open_floats(window_keys)
+
+  -- We need to redraw here, otherwise the floats won't show up until after the
+  -- getchar() call.
+  vim.cmd('redraw')
+
+  local char = fn.getchar()
+  local key = fn.nr2char(char)
+  local window = window_keys[key]
+
+  -- Only ask for the second character if we actually have a pair that starts
+  -- with the first character.
+  if not window and char ~= escape and ask_second_char(window_keys, key) then
+    window = window_keys[key .. fn.nr2char(fn.getchar())]
+  end
+
+  for window, bufnr in pairs(floats) do
+    api.nvim_win_close(window, true)
+    api.nvim_buf_delete(bufnr, { force = true })
+  end
+
+  if window then
+    api.nvim_set_current_win(window)
+  end
+end
+
+return M
